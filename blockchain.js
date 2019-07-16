d2d5506f4c82b6542fb14836d04c93fa2c056e36
The Nonexistent <thenonexistent666@gmail.com>
2019-07-17 03:45:47 +0430

    Airdrop

--- blockchain.js
+++ blockchain.js
@@ -3,14 +3,26 @@ const Block = require('./block.js');
 
 class BlockChain
 {
-    constructor(difficulty, reward)
+    constructor(difficulty, reward, initialcoins)
     {
         this.chain  = [];
-        this.difficulty = difficulty;
         this.unminedtransactions = [];
+
+        this.difficulty = difficulty;
         this.reward = reward;
 
+        this.registered = ["#1", "#2", "#4", "#123"];//Initially Registered Wallets. Will Be Airdropped.
+
         this.creategenesis();
+        this.airdrop(initialcoins);
+    }
+
+    airdrop(coins)
+    {
+        for (const wallet of this.registered)
+        {
+            this.unminedtransactions.push(new Transaction(Date.now(), "mint", wallet, coins));
+        }
     }
 
     creategenesis()
@@ -27,6 +39,7 @@ class BlockChain
 
     minecurrent(miner)
     {
+        console.log("Mining A Block...");
         let block = new Block(Date.now(), this.unminedtransactions, this.getlatest().hash);
         block.mine(this.difficulty);
 
--- main.js
+++ main.js
@@ -4,15 +4,17 @@ const BlockChain = require('./blockchain.js');
 
 let difficulty = 4;
 let reward = 40;
+let initalcoin = 5;
 
-let chain = new BlockChain(difficulty, reward);
+let chain = new BlockChain(difficulty, reward, initalcoin);
+
+chain.minecurrent("#123");
 
 console.log("Making A New Transaction...");
 chain.make(new Transaction(Date.now(), "#1" , "#123", 20));
 console.log("Making A New Transaction...");
 chain.make(new Transaction(Date.now(), "#123" , "#1", 15));
 
-console.log("Mining A Block...");
 chain.minecurrent("#1");
 
 chain.minecurrent("#4");
@@ -24,3 +26,4 @@ console.log("Valid? ", chain.isvalid().toString());
 
 console.log("#1 Balance:" + chain.getbalance("#1"));
 console.log("#123 Balance:" + chain.getbalance("#123"));
+console.log("#4 Balance:" + chain.getbalance("#4"));
